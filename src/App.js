bb44ac0fd18c95f996e5da51053c74563eba10d5
d2askif <daniel.shenkutie@gmail.com>
2020-02-16 14:07:02 +0100

    Headline component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../src/components/header';
+import Headline from '../src/components/headline';
+
 import './app.scss';
 
 class App extends React.Component {
@@ -7,6 +9,9 @@ class App extends React.Component {
     return (
       <div>
         <Header></Header>
+        <section className='main'>
+          <Headline header={'Posts'} desc='Culpa veniam fugiat aliqua mollit excepteur. Labore ex do ad commodo pariatur et excepteur ea non esse. Officia veniam esse eiusmod laborum do excepteur commodo enim excepteur. Ut nulla pariatur reprehenderit cillum deserunt sit laborum veniam eiusmod. Incididunt quis mollit id tempor id id do id irure aliquip qui proident et enim. Anim cupidatat consequat officia mollit ad.'/>
+        </section>
       </div>
     );
   }
