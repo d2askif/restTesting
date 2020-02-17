596983e364b8e73a4550fd5ff81c3dad67734746
d2askif <daniel.shenkutie@gmail.com>
2020-02-17 23:10:11 +0100

    button

---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ShareButton extends Component {
+  render() {
+    return <button></button>;
+  }
+}
+
+ShareButton.protoType = {};
+
+export default ShareButton;
